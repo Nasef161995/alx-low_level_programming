0eb357cbbc3a156b3d6d95ed977d77f19ce73cca
Nasef161995 <mnasef905@gmail.com>
2023-06-15 16:31:37 -0700

    task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main-Entry point 
  *
  * Description: A C program that prints with printf function.
  *
- * Return: Always 0 (success) 
+ * Return:Always 0 (success) 
 */
 
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return 0;
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
 }
