502ea890ea964ee69fb6b969001af21105efbdc7
Nasef161995 <mnasef905@gmail.com>
2023-06-15 16:26:54 -0700

    task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main -Entry point 
+ * main-Entry point 
  *
  * Description: Using sizeof to print the size of variase type.
  *
- * Return:Always 0 (success) 
+ * Return:Always 0(success) 
 */
 
 int main(void)
@@ -15,5 +15,5 @@ printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
-return (0);   
+return(0);   
 }
