496bc8bf703f4700051d853e93f76fa7b651ba24
Nasef161995 <mnasef905@gmail.com>
2023-06-15 16:53:35 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,11 @@
 /**
  * main - Entry point
  *
- * Desreption: A C program that prints with puts function 
- *
- * Return: Always 0 (success) 
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {   
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }	
