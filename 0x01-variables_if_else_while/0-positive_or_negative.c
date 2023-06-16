67fb52a8a1597bbf91fad1e969fe2a887e80d33e
Nasef161995 <mnasef905@gmail.com>
2023-06-16 10:01:25 -0700

    task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,13 +12,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("%d ", n);
-	if (n > 0) {
-	printf(" is positive");
+	if (n > 0)
+	{
+	printf("is positive");
 	}
-	else if (n == 0) { 
+	else if (n == 0) 
+	{ 
 	printf("is zero");
 	}
-	else {
+	else
+	{
 	printf("is negative");
 	}
 	return (0);
