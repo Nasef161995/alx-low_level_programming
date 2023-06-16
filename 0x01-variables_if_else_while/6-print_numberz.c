fd5469851526a8bcfc0cd4d8182b7fba736c6109
Nasef161995 <mnasef905@gmail.com>
2023-06-16 16:30:07 -0700

    task

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-    int i = 0;
-    while (i < 10) {
-        putchar(i + '0');
-        i++;
-    }
-    putchar('\n');
-    return 0;
+	int i = 0;
+	while (i < 10) {
+	putchar(i + '0');
+	i++;
+	}
+	putchar('\n');
+	return 0;
 }
