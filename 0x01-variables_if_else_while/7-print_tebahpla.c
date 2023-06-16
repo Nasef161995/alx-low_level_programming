5fd5a706973a60a82a5eeed5051c73ea43f45f59
Nasef161995 <mnasef905@gmail.com>
2023-06-16 16:34:32 -0700

    task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 'z';
+	while (i >= 'a') {
+	putchar(i);
+	i--;
+	}
+	putchar('\n');
+	return 0;
+}
