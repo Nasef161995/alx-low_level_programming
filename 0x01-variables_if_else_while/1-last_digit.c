a481b40fe1b7f9d5f20cfffcb7ae468f677238aa
Nasef161995 <mnasef905@gmail.com>
2023-06-16 13:00:59 -0700

    task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,6 +11,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = abs(n) % 10;
+	if (n < 0)
+	{
+	last_digit = -1 * (n % 10);
+	}
+	else
+	{
+	last_digit = n % 10;
+	}
 	printf("Last digit of %d is %d ", n, last_digit);
 	if (last_digit > 5)
 	{
