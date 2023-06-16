edc112d069291269ef13db3b90f596e36079a91e
Nasef161995 <mnasef905@gmail.com>
2023-06-15 18:23:21 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - This is the entry point of the project
  *
- * Return: Always 0 (Success)
+ * Return: always 0
  */
 int main(void)
-{   
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}	
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
