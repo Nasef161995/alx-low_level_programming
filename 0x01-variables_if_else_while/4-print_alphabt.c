eb24bd1f0c064fc96947ae7ecde2e1c6fd840e59
Nasef161995 <mnasef905@gmail.com>
2023-06-16 15:57:36 -0700

    task

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        char c = 'a';
-        while (c <= 'z');
-	{
-        putchar(c);
+    char c = 'a';
+
+    while (c <= 'z') {
+        if (c != 'e' && c != 'q') {
+            putchar(c);
+        }
         c++;
-	}
-	{
-	if (c != 'e' && c!= 'q');
-	}
-        return (0);
+    }
+
+    putchar('\n');
+
+    return (0);
 }
