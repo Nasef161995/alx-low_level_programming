278c132b303430d46c6dfb82b6585debf70fd6cd
Nasef161995 <mnasef905@gmail.com>
2023-06-16 15:29:32 -0700

    task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char c = 'a';
+        while (c <= 'z') {
+	if (c != 'e' && c!= 'q') }
+	{
+        putchar(c);
+        c++;
+        }
+        return (0);
+}
