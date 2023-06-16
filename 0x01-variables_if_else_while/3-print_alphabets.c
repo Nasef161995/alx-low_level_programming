512ab3b4217d86ab70df2a27c9b5bbdb2206d553
Nasef161995 <mnasef905@gmail.com>
2023-06-16 14:42:25 -0700

    task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+	while (c < = 'z')
+	{
+	putchar(c);
+	c++;
+	}
+	char c = 'A';
+	while (c < = 'Z');
+	{
+	putchar(c);
+	c++;
+	}
+	return (0);
+}
+
