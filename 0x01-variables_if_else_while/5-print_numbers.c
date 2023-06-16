01429f01d568b36705285b42c2677b989efbb939
Nasef161995 <mnasef905@gmail.com>
2023-06-16 16:11:32 -0700

    task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	while (i < 10) {
+	printf("%d", i);
+	i++;
+	}
+	printf("\n");
+	return (0);
+}
