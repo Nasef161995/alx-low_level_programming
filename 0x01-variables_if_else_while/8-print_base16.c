502eb3eab326a8db02f25834bc8b139e7b6a9cf4
Nasef161995 <mnasef905@gmail.com>
2023-06-16 16:42:30 -0700

    task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */ 
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++) {
+	putchar(i + '0');
+	}
+	for (i = 0; i < 6; i++) {
+	putchar(i + 'a');
+	}
+	putchar('\n');
+}
