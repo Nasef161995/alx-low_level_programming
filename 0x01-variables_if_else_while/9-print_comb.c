b564a98cd6b782c38889c1d07dd6ba08a86a8e05
Nasef161995 <mnasef905@gmail.com>
2023-06-16 16:55:09 -0700

    task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,12 +5,13 @@
  */
 int main(void)
 {
-    int i;
-    for (i = 0; i < 10; i++) {
+    int i = 0;
+    while (i < 10) {
         putchar(i + '0');
         if (i < 9) {
             putchar(', ');
         }
+        i++;
     }
     putchar('\n');
     return 0;
