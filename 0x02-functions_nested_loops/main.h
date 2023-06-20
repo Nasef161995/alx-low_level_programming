76417f1b0c35af44174a2b62edd23b30618a76c3
Nasef161995 <mnasef905@gmail.com>
2023-06-20 10:05:07 -0700

    task

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,11 +4,12 @@
  * Return: Always 0 (Success)
  */
 void print_alphabet();
+{
 char c = 'a';
-while (c <= 'z') {
+while (c <= 'z')
+{
 putchar(c);
 c++;
 }
-    putchar('\n');
+putchar('\n');
 }
-
