49cc7edd0573d64427ee4d53614ca0599dc69a08
Nasef161995 <mnasef905@gmail.com>
2023-06-20 16:16:41 -0700

    task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,7 +5,15 @@
  * Return: Always 0 (Success)
  */
 int print_last_digit(int n) {
-    int last_digit = _abs(n % 10);
+    int last_digit;
+
+    if (n < 0) {
+        last_digit = (n % 10) * -1;
+    } else {
+        last_digit = n % 10;
+    }
+
     _putchar(last_digit);
+
     return last_digit;
 }
