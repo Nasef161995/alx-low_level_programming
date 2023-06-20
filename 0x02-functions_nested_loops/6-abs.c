30aeda69868130493f6f3758969ca9a90a184e5c
Nasef161995 <mnasef905@gmail.com>
2023-06-20 15:59:21 -0700

    task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int _abs(int n) {
+    if (n < 0) {
+        return -n;
+    } else {
+        return n;
+    }
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,6 +14,7 @@ int _isalpha(int c);
 
 int print_sign(int n);
 
+int _abs(int);
 
 
 
