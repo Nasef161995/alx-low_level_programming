fa304c195424dcf383027af7b11be1c98bbbf251
Nasef161995 <mnasef905@gmail.com>
2023-06-20 16:35:56 -0700

    task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+//#include "main.h"
+
+void print_to_98(int n) {
+    int i;
+
+    if (n <= 98) {
+        for (i = n; i <= 98; i++) {
+            int num = i;
+            if (num < 0) {
+                _putchar('-');
+                num *= -1;
+            }
+            if (num >= 10) {
+                print_to_98(num / 10);
+                print_to_98(num % 10);
+            } else {
+                _putchar(num + '0');
+            }
+            if (i != 98) {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+    } else {
+        for (i = n; i >= 98; i--) {
+            int num = i;
+            if (num < 0) {
+                _putchar('-');
+                num *= -1;
+            }
+            if (num >= 10) {
+                print_to_98(num / 10);
+                print_to_98(num % 10);
+            } else {
+                _putchar(num + '0');
+            }
+            if (i != 98) {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+    }
+
+    _putchar('\n');
+}
