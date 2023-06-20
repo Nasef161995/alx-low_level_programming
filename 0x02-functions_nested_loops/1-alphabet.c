76da26f2f4ce38a0cbd26215d21d6871a00e74c0
Nasef161995 <mnasef905@gmail.com>
2023-06-20 09:27:29 -0700

    task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,18 +3,18 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-void print_alphabet()
-{
-char c = ('a');
-while (c <= 'z')
-{
-putchar (c);
-c++;
+
+void print_alphabet() {
+    char c = 'a';
+    while (c <= 'z') {
+        putchar(c);
+        c++;
+    }
+    putchar('\n');
 }
-putchar('\n');
+
+int main() {
+    print_alphabet();
+    return 0;
 }
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+
