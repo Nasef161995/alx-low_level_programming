1e86bfeb4bab3f1899a9275e1d2e857d5e9a9041
Nasef161995 <mnasef905@gmail.com>
2023-06-20 16:11:03 -0700

    task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n) {
+    int last_digit = _abs(n % 10);
+    _putchar(last_digit + '0');
+    return last_digit;
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,7 +16,7 @@ int print_sign(int n);
 
 int _abs(int);
 
-
+int print_last_digit(int);
 
 
 #endif
