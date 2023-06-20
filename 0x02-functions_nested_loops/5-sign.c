a0f1c831ee2af3efc0d800201316abdef1a34454
Nasef161995 <mnasef905@gmail.com>
2023-06-20 15:47:12 -0700

    task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,21 +4,15 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-int print_sign(int n)
-{
-if (n > 0 )
-{
-return 1;
-_putchar('+');
-}
-else if (n = 0)
-{
-return 0;
-_putchar('0');
-}
-else 
-{
-return -1;
-_putchar('-1');
-}
+int print_sign(int n) {
+    if (n > 0) {
+        _putchar('+');
+        return 1;
+    } else if (n == 0) {
+        _putchar('0');
+        return 0;
+    } else {
+        _putchar('-');
+        return -1;
+    }
 }
