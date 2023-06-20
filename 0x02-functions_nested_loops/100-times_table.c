2c88ad7b430b3e5c22534d310765906a3c6bf21c
Nasef161995 <mnasef905@gmail.com>
2023-06-20 16:48:16 -0700

    task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,23 +9,31 @@ void print_times_table(int n) {
         return;
     }
 
-    int i, j;
+    int i, j, result;
 
     for (i = 0; i <= n; i++) {
         for (j = 0; j <= n; j++) {
-            int result = i * j;
+            result = i * j;
             if (j == 0) {
                 _putchar('0');
-                _putchar(',');
-                _putchar(' ');
             } else {
-                _putchar(result / 100 + '0');
-                _putchar((result / 10) % 10 + '0');
-                _putchar(result % 10 + '0');
-                if (j != n) {
-                    _putchar(',');
+                _putchar(' ');
+                if (result < 10) {
+                    _putchar(' ');
+                    _putchar(' ');
+                } else if (result < 100) {
                     _putchar(' ');
                 }
+            }
+            if (result >= 100) {
+                _putchar(result / 100 + '0');
+                _putchar((result / 10) % 10 + '0');
+            } else if (result >= 10) {
+                _putchar(result / 10 + '0');
+            }
+            _putchar(result % 10 + '0');
+            if (j != n) {
+                _putchar(',');
             }
         }
         _putchar('\n');
