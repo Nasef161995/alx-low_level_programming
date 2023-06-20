5c7d3ec52658f701c5a09ebc5df28e65e4ae96f1
Nasef161995 <mnasef905@gmail.com>
2023-06-20 16:41:36 -0700

    task

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+void print_times_table(int n) {
+    if (n < 0 || n > 15) {
+        return;
+    }
+
+    int i, j;
+
+    for (i = 0; i <= n; i++) {
+        for (j = 0; j <= n; j++) {
+            int result = i * j;
+            if (j == 0) {
+                _putchar('0');
+            } else {
+                if (result < 10) {
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar(result + '0');
+                } else if (result < 100) {
+                    _putchar(' ');
+                    _putchar(result / 10 + '0');
+                    _putchar(result % 10 + '0');
+                } else {
+                    _putchar(result / 100 + '0');
+                    _putchar((result / 10) % 10 + '0');
+                    _putchar(result % 10 + '0');
+                }
+            }
+            if (j != n) {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }
+}
