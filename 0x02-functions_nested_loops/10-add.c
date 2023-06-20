fa6ae8b84fffdae82e4f5f65b217e729514f667a
Nasef161995 <mnasef905@gmail.com>
2023-06-20 16:32:37 -0700

    task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int add(int num1, int num2) {
+    int result = num1 + num2;
+    return result;
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -22,7 +22,7 @@ void jack_bauer(void);
 
 void times_table(void);
 
-
+int add(int, int);
 
 
 #endif
