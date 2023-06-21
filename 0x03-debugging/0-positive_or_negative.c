39d08e467d2569a44e2628644882e6e261a7eafa
Nasef161995 <mnasef905@gmail.com>
2023-06-21 10:27:43 -0700

    task

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d ", n);
+	if (n > 0)
+	{
+	printf("is positive");
+	}
+	else if (n == 0)
+	{
+	printf("is zero");
+	}
+	else
+	{
+	printf("is negative");
+	}
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+
+
+
+
+
+
+#endif
