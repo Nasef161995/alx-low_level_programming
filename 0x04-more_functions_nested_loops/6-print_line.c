efaf3fd063a79ac9e12ca8fe65e898a97ebe99d9
Nasef161995 <mnasef905@gmail.com>
2023-06-22 16:34:47 -0700

    task

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,17 +8,17 @@
  */
 void print_line(int n)
 {
-    if (n <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        int i;
-        for (i = 0; i < n; i++)
-        {
-            _putchar('_');
-        }
-        _putchar('\n');
-    }
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
 }
