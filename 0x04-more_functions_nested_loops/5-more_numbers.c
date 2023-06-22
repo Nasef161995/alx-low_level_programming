9ad9f8e4deeda39b8bb28178e075decd82a8c71a
Nasef161995 <mnasef905@gmail.com>
2023-06-22 15:46:03 -0700

    task

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - is function is for print numbers
+ *
+ * parameter is an integer
+ *
+ * Return: returns 0
+ */
+void more_numbers(void)
+{
+int i;
+for (i = 0; i <= 14; i++)
+{
+_putchar(i + '0');
+}
+int j;
+for (j = 0; j <= 14; j++)
+{
+_putchar(j + '0');
+}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,7 +14,7 @@ void print_numbers(void);
 
 void print_most_numbers(void);
 
-
+void more_numbers(void);
 
 
 
