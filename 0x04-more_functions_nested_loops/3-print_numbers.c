03eac7b46497beae90f51d39054938d00ddaab34
Nasef161995 <mnasef905@gmail.com>
2023-06-22 12:25:30 -0700

    task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - is function is for print numbers
+ *
+ * parameter is an integer
+ *
+ * Return: returns 0
+ */
+
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9, i++)
+{
+_putchar(i);
+_putchar('\n);
+}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,7 +10,7 @@ int _isdigit(int c);
 
 int mul(int a, int b);
 
-
+void print_numbers(void);
 
 
 
