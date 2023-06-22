8701916e6c9d1e783ee6de257537b94ea2ed8391
Nasef161995 <mnasef905@gmail.com>
2023-06-22 12:50:23 -0700

    task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -11,7 +11,7 @@
 void print_numbers(void)
 {
 int i;
-char digits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '\n'};
+char digits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 for (i = 0; i < sizeof(digits); i++)
 {
 _putchar(digits[i]);
