6462a17d2cf98d836453faca9c0cbe8b3f0b9052
Nasef161995 <mnasef905@gmail.com>
2023-06-22 13:40:26 -0700

    task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,12 +6,12 @@
  *
  * Return: returns 0
  */
-
 void print_numbers(void)
 {
 int i;
-for (i = 0, i <= 9; i++)
+for (i = 0; i <= 9; i++)
 {
-_putchar('i');
+_putchar(i + '0');
 }
+_putchar('\n');
 }
