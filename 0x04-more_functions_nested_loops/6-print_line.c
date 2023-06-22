5703487b894a7f322f5a1981557b12237bb07037
Nasef161995 <mnasef905@gmail.com>
2023-06-22 16:22:32 -0700

    task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - is function that draws a straight line in the terminal
+ *
+ *@n: parameter is an integer
+ *
+ * Return: returns 0
+ */
+void print_line(int n)
+{
+int n;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,7 +16,7 @@ void print_most_numbers(void);
 
 void more_numbers(void);
 
-
+void print_line(int n);
 
 
 #endif /* MAIN_H */
