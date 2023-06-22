6663ac0d7f2d51f32068c3794af75cae625a627e
Nasef161995 <mnasef905@gmail.com>
2023-06-22 15:59:05 -0700

    task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,17 @@
  */
 void more_numbers(void)
 {
-int i;
-for (i = 0; i <= 14; i++)
+int i, j;
+for (i = 0; i < 10; i++)
 {
-_putchar(i + '0');
-}
-int j;
 for (j = 0; j <= 14; j++)
 {
-_putchar(j + '0');
+if (j > 9)
+{
+_putchar('1');
+}
+_putchar((j % 10) + '0');
+}
+_putchar('\n');
 }
 }
