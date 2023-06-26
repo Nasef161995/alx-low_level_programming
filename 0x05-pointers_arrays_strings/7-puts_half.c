755dfbe2428f3acaee6f81934652e3625d923b46
Nasef161995 <mnasef905@gmail.com>
2023-06-26 16:51:21 -0700

    task

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,15 +9,25 @@
 
 void puts_half(char *str)
 {
-int len, i;
+int len, n, i;
 len = 0;
 while (str[len] != '\0')
 {
 len++;
 }
-for (i = len / 2; i < len; i++)
+if (len % 2 == 0)
+{
+for (i = len / 2; str[i] != '\0'; i++)
 {
 _putchar(str[i]);
 }
+}
+else
+{
+for (n = (len - 1) / 2; n < len - 1; n++)
+{
+_putchar(str[n + 1]);
+}
 _putchar('\n');
+}
 }
