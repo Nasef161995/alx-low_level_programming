0ba7acd90456e3c6187c130cef2317fa38f61d3a
Nasef161995 <mnasef905@gmail.com>
2023-06-26 14:55:29 -0700

    task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - function that reverses a string
+ *
+ * @s: parameter is a string
+ *
+ * Return: returns (0).
+ */
+
+void rev_string(char *s);
+{
+int c = 0;
+while (s[c] != '\0')
+{
+c++;
+}
+for (c -= 1; c >= 0; c--)
+{
+_putchar(s[c]);
+}
+_putchar('\n');
+}
