fd7652ea211da4b2b2336eb84c15e7b4f30a9f07
Nasef161995 <mnasef905@gmail.com>
2023-06-26 16:11:32 -0700

    task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts2 - function that reverses a string
+ * puts2 - function that prints every other character of a string, starting with the first character
  *
  * @str: parameter is a charecter
  *
