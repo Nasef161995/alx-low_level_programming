a099f5a3a69f92e971ee93ffa4f1b348d857be08
Nasef161995 <mnasef905@gmail.com>
2023-06-29 12:33:59 -0700

    task

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,7 +8,7 @@
  *
  * @n: parameter is string
  *
- * Return: returns (0).
+ * Return: returns (dest).
  */
 
 char *_strncpy(char *dest, char *src, int n)
@@ -22,16 +22,5 @@ for ( ; i < n; i++)
 {
 dest[i] = '\0';
 }
-return (dest);
-}
-
-
-
-
-
-
-
-
-
 return (dest);
 }
