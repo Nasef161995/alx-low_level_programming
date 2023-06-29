4bedc032cafa6e7568390b7893c0aa2e27980723
Nasef161995 <mnasef905@gmail.com>
2023-06-29 13:50:12 -0700

    task

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,11 +11,18 @@
 
 void reverse_array(int *a, int n)
 {
-int i, j;
-for (i = 0; j = n - 1; i < j; i++; j--)
+int i;
+int j;
+int temp;
+i = 0;
+j = n - 1;
+while (i < j)
 {
-int temp = a[i];
+temp = a[i];
 a[i] = a[j];
 a[j] = temp;
+i++;
+j--;
 }
 }
+
