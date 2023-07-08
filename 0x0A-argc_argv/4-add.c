54a0fe6eb6e311a58880416897bade8ed3f54f9c
Nasef161995 <mnasef905@gmail.com>
2023-07-08 15:54:26 -0700

    task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,19 +13,20 @@
  */
 int main(int argc, char *argv[])
 {
-int i, j, sum;
-sum = 0;
+	int i, j, add = 0;
 
-for (i = 1; i < argc; i++)
-{
-for (j = 0; argv[i][j] != '\0'; j++)
-{
-if (!isdigit(argv[i][j]))
-{
-return (puts("Error"), 1);
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
 }
-}
-sum += atoi(argv[i]);
-}
-return (printf("%d\n", sum), 0);
-
