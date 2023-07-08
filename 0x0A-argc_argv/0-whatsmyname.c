31072a744e71cebcf31fa32eaf119835767fcd0a
Nasef161995 <mnasef905@gmail.com>
2023-07-08 08:35:26 -0700

    task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints its name, followed by a new line.
+ *
+ * @argc: number of command line arguments.
+ *
+ * @argv: array that contains the program command line arguments.
+ *
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+printf("%s", argv[0]);
+return (0);
+}
