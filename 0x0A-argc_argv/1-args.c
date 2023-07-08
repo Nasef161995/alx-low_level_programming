6f44b6896371ad15b3b84d6634e073425f21f91c
Nasef161995 <mnasef905@gmail.com>
2023-07-08 12:19:11 -0700

    task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,7 +9,7 @@
  *
  * Return: 0 - success.
  */
-int main(int argc __attribute__((unused)), char **argv)
+int main(int argc __attribute__((unused)), char *argv[])
 {
 printf("%d\n", argc);
 return (0);
