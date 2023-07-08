7d66ce5783395a1584a122cbcd504d3dfbe10163
Nasef161995 <mnasef905@gmail.com>
2023-07-08 13:06:05 -0700

    task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ *
+ * @argc: number of command line arguments.
+ *
+ * @argv: array that contains the program command line arguments.
+ *
+ * Return: 0 - success.
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+if (argc > 1)
+{
+int i;
+int mult = 1;
+for (i = 1; i < argc; i++)
+{
+mult = mult *atoi(argv[i]);
+}
+printf("%d\n", mult);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
+}
