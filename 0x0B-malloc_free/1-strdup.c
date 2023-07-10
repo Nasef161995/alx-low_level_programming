19534e9202072131e6fc1db8f1156e5602885c81
Nasef161995 <mnasef905@gmail.com>
2023-07-10 16:08:09 -0700

    task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,14 +13,16 @@ char *_strdup(char *str)
 {
 if (str == NULL)
 return (NULL);
-unsigned int len = 0;
+int len;
+unsigned int i;
 char *newstr;
+len = 0;
 while (str[len] != '\0')
 {
 len++;
 }
 newstr = malloc((len + 1) * sizeof(char));
-for (unsigned int i = 0; i <= len; i++)
+for (i = 0; i <= len; i++)
 {
 newstr[i] = str[i];
 }
