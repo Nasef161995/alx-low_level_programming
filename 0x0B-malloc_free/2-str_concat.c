3df0727ffb0bcdcdcb3d13214bc85d3a77d24d90
Nasef161995 <mnasef905@gmail.com>
2023-07-10 17:34:05 -0700

    task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,9 +9,32 @@
  *
  * @s2: parameter is string
  *
- * Return: returns pointer 
+ * Return: pointer to newly allocated space in memory
  */
 
 char *str_concat(char *s1, char *s2)
 {
-
+char *newstr;
+unsigned int i, j;
+if (s1 == NULL)
+	return (NULL);
+if (s2 == NULL)
+	return (NULL);
+for (i = 0; s1[i] != '\0'; i++)
+	;
+for (j = 0; s2[j] != '\0'; j++)
+	;
+newstr = (char *)malloc(sizeof(char) * (i + 1 + j + 1));
+if (newstr == NULL)
+	return (NULL);
+unsigned int x, y;
+for (x = 0; x <= i; x++)
+{
+newstr[x] = s1[x];
+}
+for (y = 0; y <= j; y++)
+{
+newstr[i + y] = s2[y];
+}
+return (newstr);
+}
