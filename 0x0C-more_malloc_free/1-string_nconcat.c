5b4d9042e821f04c84bb165de7f853d55a50c504
Nasef161995 <mnasef905@gmail.com>
2023-07-13 13:09:11 -0700

    task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 /**
@@ -20,40 +21,36 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 unsigned int len1, len2, len3, size, i, j;
 char *nstr;
 if (s1 == NULL)
-	s1 = "";
+s1 = "";
 if (s2 == NULL)
-	s2 = "";
-
+s2 = "";
 len1 = 0;
 while (s1[len1] != '\0')
-	len1++;
+len1++;
 len2 = 0;
 while (s2[len2] != '\0')
-	len2++;
+len2++;
 if (n >= len2)
-	len3 = len2;
+len3 = len2;
 else
-	len3 = n;
+len3 = n;
 size = len1 + len3;
 nstr = malloc((sizeof(char) * size) +1);
-
 if (nstr == NULL)
-	return (NULL);
-
-	i = 0;
-	while (i < len1)
-	{
-	nstr[i] = s1[i];
-	i++;
-	}
-
-	j = 0;
-	while (j < len3)
-	{
-	nstr[i] = s2[j];
-	i++;
-	j++;
-	}
+return (NULL);
+i = 0;
+while (i < len1)
+{
+nstr[i] = s1[i];
+i++;
+}
+j = 0;
+while (j < len3)
+{
+nstr[i] = s2[j];
+i++;
+j++;
+}
 nstr[i] = '\0';
 return (nstr);
 free(nstr);
