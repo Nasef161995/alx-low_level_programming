f3325ee64da3d4c76254d93a108fb82fc10a97b9
Nasef161995 <mnasef905@gmail.com>
2023-07-13 17:06:04 -0700

    task

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,43 +1,50 @@
-#include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * _realloc - function that concatenates two strings.
- * @old_size: number min.
- * @new_size: number max.
- * @s: array input.
- * Return: Always 0.
+ * *_realloc - reallocates a memory block
+ * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old_size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block.
+ *
+ * Return: pointer to the newly allocated memory block
  */
-void *_realloc(void *s, unsigned int old_size, unsigned int new_size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *str;
-	char *ptr = s;
+	char *ptr1;
+	char *old_ptr;
 	unsigned int i;
 
-	if (new_size == 0 && ptr != NULL)
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (new_size == old_size)
-	{
-		return (ptr);
-	}
-	if (ptr == NULL)
-	{
-		str = malloc(new_size);
-		return (str);
-	}
-	str = malloc(new_size);
-	if (str == NULL)
-	{
+
+	if (!ptr)
+		return (malloc(new_size));
+
+	ptr1 = malloc(new_size);
+	if (!ptr1)
 		return (NULL);
-	}
-	for (i = 0; i < old_size; i++)
+
+	old_ptr = ptr;
+
+	if (new_size < old_size)
 	{
-		*(str + i) = *(ptr + i);
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
 	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
 	free(ptr);
-	return (str);
+	return (ptr1);
 }
