16fb9047d16c24bbcd59352f2a898a9eb4b8c8d1
Nasef161995 <mnasef905@gmail.com>
2023-07-17 14:24:00 -0700

    task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - is a dog structure
+ * @name: First member is name
+ * @age: Second member is age
+ * @owner: Third member is owner
+ *
+ * Description: structure dog just a lone dog struct in a big kitty world
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
