d61ae6c35ef711431f06ded43293f6b8e2f0804a
Nasef161995 <mnasef905@gmail.com>
2023-07-17 17:05:40 -0700

    task

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,5 +18,8 @@ printf("Name: (nil)");
 else
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
+if (d->owner == NULL)
+printf("Owner: (nil)");
+else
 printf("Owner: %s\n", d->owner);
 }
