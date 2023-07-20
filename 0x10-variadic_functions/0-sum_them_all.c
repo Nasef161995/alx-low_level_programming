5703792086636efbfd776938a0c7792516f37a60
Nasef161995 <mnasef905@gmail.com>
2023-07-20 11:34:51 -0700

    task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,19 +10,21 @@
  * Return: returns the sum of all its parameters
  */
 
+
 int sum_them_all(const unsigned int n, ...)
 {
 if (n == 0)
+{
 return (0);
+}
 va_list args;
 va_start(args, n);
-int i;
-int sum = 0;
+unsigned int i;
+unsigned int sum = 0;
 for (i = 0; i < n; i++)
 {
 sum = sum + va_arg(args, int);
 }
-
 va_end(args);
 return (sum);
 }
