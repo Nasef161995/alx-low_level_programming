05869e45695eed7e66c1d5b91b0abc518cb6f84a
Nasef161995 <mnasef905@gmail.com>
2023-07-20 15:29:09 -0700

    task

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,16 +26,18 @@ for (i = 0; i < n ; i++)
 {
 str = va_arg(strings, char*);
 if (str == NULL)
+{
 printf("nil");
+}
 else
+}
 printf("%s", str);
-
+}
 if (i != n - 1 && separator != NULL)
 {
 printf("%s", separator);
 }
-
 }
-printf("\n");
 va_end(strings);
+printf("\n");
 }
