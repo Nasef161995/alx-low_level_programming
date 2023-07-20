a8e0124e07c627ddabd79ed894ca2bd8ba577803
Nasef161995 <mnasef905@gmail.com>
2023-07-20 13:10:52 -0700

    task

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,7 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+
 /**
  * print_numbers - function that  prints numbers
  *
@@ -13,15 +14,15 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-va_list numbers;
-va_start(numbers, n);
+va_list num;
 unsigned int i;
+va_start(num, n);
 for (i = 0; i < n ; i++)
 {
-printf("%d", va_arg(numbers, int));
+printf("%d", va_arg(num, unsigned int));
 if (i < n - 1 && separator != NULL)
 printf("%s", separator);
 }
-va_end(numbers);
+va_end(num);
 printf("\n");
 }
