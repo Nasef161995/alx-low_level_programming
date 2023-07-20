9ff6e00b08e6bc57f70571f6f1b10dcc3429af79
Nasef161995 <mnasef905@gmail.com>
2023-07-19 17:12:51 -0700

    task

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,11 +20,7 @@ op_t ops[] = {
 	{ NULL, NULL }
 	};
 int i = 0;
-while (i < 5)
-{
-if (strcmp(s, ops[i].op) == NULL)
-return (ops[i].f);
+while (ops[i].op != NULL && *(ops[i].op) != *s)
 i++;
-}
-return (0);
+return (ops[i].f);
 }
