8a59a16e550073d85e0c8e81204c175218eca6a3
Nasef161995 <mnasef905@gmail.com>
2023-07-20 15:31:49 -0700

    task

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,28 +16,28 @@
  * Return: returns void
  */
 
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 va_list strings;
 char *str;
-unsigned int i;
+unsigned int index;
+
 va_start(strings, n);
-for (i = 0; i < n ; i++)
+
+for (index = 0; index < n; index++)
 {
-str = va_arg(strings, char*);
+str = va_arg(strings, char *);
+
 if (str == NULL)
-{
-printf("nil");
-}
+printf("(nil)");
 else
-}
 printf("%s", str);
-}
-if (i != n - 1 && separator != NULL)
-{
+
+if (index != (n - 1) && separator != NULL)
 printf("%s", separator);
 }
-}
-va_end(strings);
+
 printf("\n");
+va_end(strings);
 }
