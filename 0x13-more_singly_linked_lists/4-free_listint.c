0572d25b7eea51c342338105ad980c08c4ac61f2
Nasef161995 <mnasef905@gmail.com>
2023-07-31 13:06:20 -0700

    more Singly linked list task

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -13,11 +13,10 @@
 void free_listint(listint_t *head)
 {
 listint_t *ptr;
-ptr = head;
-while (ptr != NULL)
+while (head != NULL)
 {
 ptr = ptr->next;
-free(ptr->next);
-}
 free(head);
+head = ptr;
+}
 }
