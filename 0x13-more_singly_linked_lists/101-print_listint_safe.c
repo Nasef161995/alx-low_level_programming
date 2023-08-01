114d4a8aa53af8472ad4da0c6c5c93eb51754228
Nasef161995 <mnasef905@gmail.com>
2023-08-01 15:40:18 -0700

    more Singly linked list task

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,65 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _r - reallocates memory for an array
+ *
+ * @list: the old list to append
+ * @size: size of the new list
+ * @new: new node to add
+ *
+ * Return: returns pointer
+ */
+
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
+{
+	const listint_t **newlist;
+	size_t i;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
+}
+
+/**
+ * print_listint_safe - prints a list
+ *
+ * @head: pointer to node
+ *
+ * Return: returns the number of nodes
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t i, num = 0;
+	const listint_t **list = NULL;
+
+	while (head != NULL)
+	{
+		for (i = 0; i < num; i++)
+		{
+			if (head == list[i])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(list);
+				return (num);
+			}
+		}
+		num++;
+		list = _r(list, num, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+	}
+	free(list);
+	return (num);
+}
