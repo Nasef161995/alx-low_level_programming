e896c5f9e044b677c019c462b893b4f4b6f592ab
Nasef161995 <mnasef905@gmail.com>
2023-08-01 00:14:57 -0700

    more Singly linked list task

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,11 +13,13 @@
  */
 int pop_listint(listint_t **head)
 {
+int n;
 if (*head == NULL)
 return (0);
+n = (*head)->n;
 listint_t *ptr;
 ptr = *head;
 *head = (*head)->next;
 free(ptr);
-return ((*head)->n);
+return (n);
 }
