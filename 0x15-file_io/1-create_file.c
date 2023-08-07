3f653876f609cdce9ef2cd1cbe3c857fd6c19d15
Nasef161995 <mnasef905@gmail.com>
2023-08-07 12:14:52 -0700

    C - File I/O task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		len++;
 	}
 	fd1 = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	fd2 = write(o, text_content, len);
+	fd2 = write(fd1, text_content, len);
 	if (fd1 == -1 || fd2 == -1)
 	return (-1);
 	close(fd1);
