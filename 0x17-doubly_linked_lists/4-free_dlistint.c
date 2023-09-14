e22094e9001cdc31034639d69af2a6281676b8ca
Nasef161995~ <mnasef905@gmail.com>
2023-09-14 15:25:34 +0300

    4444444

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - function that free list
+ * @head: pointer to double list
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *temp;
+temp = head;
+while (temp->next != 0)
+{
+free(temp->prev);
+temp = temp->next;
+}
+free(temp);
+}
