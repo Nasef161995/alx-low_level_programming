cd2f8c2122c5a9520fdb13a55d304fd49efab73c
Nasef161995~ <mnasef905@gmail.com>
2023-09-14 21:07:28 +0300

    55555

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,12 +12,19 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 dlistint_t *temp;
-unsigned int i = 0;
+unsigned int i = 0, num_node = 0;
 if (head == NULL)
 {
 return (NULL);
 }
-else if (index)
+temp = head;
+while (temp->next != NULL)
+{
+temp = temp->next;
+num_node++;
+}
+
+if (index < num_node)
 {
 temp = head;
 while (i != index)
