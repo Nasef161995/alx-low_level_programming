0e11b43a51babe2ff5c23ea4e544c7d9c8d42e88
Nasef161995~ <mnasef905@gmail.com>
2023-09-14 22:40:44 +0300

    7777777777

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,8 +1,5 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
 
+#include "lists.h"
 /**
  * insert_dnodeint_at_index - function that add node
  * @h: pointer to double list
@@ -13,7 +10,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 dlistint_t *new, *temp;
-unsigned int i = 0;
+unsigned int i = 0, total = 0;
 new = malloc(sizeof(dlistint_t));
 if (new == NULL)
 return (NULL);
@@ -25,10 +22,18 @@ new->prev = NULL;
 return (new);
 }
 temp = *h;
+while (temp->next)
+{
+temp = temp->next;
+total++;
+}
 if (idx == 0)
 add_dnodeint(h, n);
-if (temp != NULL)
+if (idx == total + 1)
+add_dnodeint_end(h, n);
+if (idx <= total)
 {
+temp = *h;
 while (i != idx - 1)
 {
 temp = temp->next;
