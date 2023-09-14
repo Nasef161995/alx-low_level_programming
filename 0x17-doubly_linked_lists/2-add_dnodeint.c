d165a112756e8795eb8dde96ac4f251a72e2a0a0
Nasef161995~ <mnasef905@gmail.com>
2023-09-14 14:49:27 +0300

    add

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,7 +13,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *new;
-new = malloc(sizeof(dlistint_t));
+new = (dlistint_t *)malloc(sizeof(dlistint_t));
 new->n = n;
 if (new == NULL)
 return (NULL);
