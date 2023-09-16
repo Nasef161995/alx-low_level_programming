7afe86b0e534eb83320ccad36123496ba7130c73
Nasef161995~ <mnasef905@gmail.com>
2023-09-16 22:51:55 +0300

    edit

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,44 +7,44 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-dlistint_t *temp;
-unsigned int i = 0;
+    dlistint_t *temp;
+    unsigned int i = 0;
 
-if (*head != NULL)
-{
+    if (*head != NULL)
+    {
 
-temp = *head;
-while (i < index)
-{
-temp = temp->next;
-i++;
-if (temp == NULL)
-return (-1);
-}
-if (index == 0)
-{
-if ((*head)->next)
-{
-*head = (*head)->next;
-(*head)->prev = NULL;
-}
-else
-{
-*head = (*head)->next;
-}
-}
-else if (temp->next == NULL)
-{
-temp->prev->next = NULL;
-}
-else
-{
-temp->prev->next = temp->next;
-temp->next->prev = temp->prev;
-}
-free(temp);
-return (1);
-}
-else
-return (-1);
+        temp = *head;
+        while (i < index)
+        {
+            temp = temp->next;
+            i++;
+            if (temp == NULL)
+                return (-1);
+        }
+        if (index == 0)
+        {
+            if ((*head)->next)
+            {
+                *head = (*head)->next;
+                (*head)->prev = NULL;
+            }
+            else
+            {
+                *head = (*head)->next;
+            }
+        }
+        else if (temp->next == NULL)
+        {
+            temp->prev->next = NULL;
+        }
+        else
+        {
+            temp->prev->next = temp->next;
+            temp->next->prev = temp->prev;
+        }
+        free(temp);
+        return (1);
+    }
+    else
+        return (-1);
 }
