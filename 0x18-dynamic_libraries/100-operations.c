9b8201c63269ec13c5bacb3812255b1c000d5685
Nasef161995~ <mnasef905@gmail.com>
2023-09-18 10:47:36 +0300

    done

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * add - function that that add
+ * @a: parameter is integer
+ * @b: parameter is integer
+ * Return: add
+ */
+int add(a, b)
+{
+return (a + b);
+}
+/**
+ * sub - function that that sub
+ * @a: parameter is integer
+ * @b: parameter is integer
+ * Return: sub
+ */
+int sub(a, b)
+{
+return (a - b);
+}
+/**
+ * mul - function that that mul
+ * @a: parameter is integer
+ * @b: parameter is integer
+ * Return: mul
+ */
+int mul(a, b)
+{
+return (a *b);
+}
+/**
+ * div - function that that div
+ * @a: parameter is integer
+ * @b: parameter is integer
+ * Return: div
+ */
+int div(a, b)
+{
+return (a / b);
+}
+/**
+ * mod - function that that mod
+ * @a: parameter is integer
+ * @b: parameter is integer
+ * Return: mod
+ */
+int mod(a, b)
+{
+return (a % b);
+}
--- 0x18-dynamic_libraries/README.md
+++ 0x18-dynamic_libraries/README.md
@@ -1,1 +1,3 @@
 Dynamic libraries
+<!-- gcc -c *.c fpic --->>> command create object files  -->
+<!-- gcc *.o -shared -o <library_name.so> -->> command create dynamic library [shared library] -->
