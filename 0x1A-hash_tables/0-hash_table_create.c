16eb13212331dc6351a424c3f8fe9b4a196892b0
Nasef161995~ <mnasef905@gmail.com>
2023-10-06 17:17:25 +0300

    00000

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,8 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_create - ........
- * @size: ...
- * Return: ...
+ * hash_table_create - function creates hash table
+ * @size: size of array
+ * Return: hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
