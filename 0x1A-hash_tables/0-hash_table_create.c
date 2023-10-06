e9c3fbd8515080a73555a4f46bcb07f4fbb83c16
Nasef161995~ <mnasef905@gmail.com>
2023-10-06 16:18:51 +0300

    000

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - ....
+ * @size: ...
+ * Return: ....
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+unsigned long int i;
+hash_table_t *table;
+table = malloc(sizeof(hash_node_t));
+if (table == NULL)
+return (NULL);
+table->size = size;
+table->array = malloc(size * sizeof(hash_node_t *));
+if (table->array == NULL)
+return (NULL);
+for (i = 0; i < table->size; i++)
+table->array[i] = NULL;
+
+return (table);
+}
