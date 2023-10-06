fad18357cdcdb736275d3ce1abe18c5a001789f0
Nasef161995~ <mnasef905@gmail.com>
2023-10-07 02:12:25 +0300

    66666

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - ...
+ * @ht: ...
+ * Return: ...
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+hash_node_t *current;
+unsigned long int i;
+
+
+
+for (i = 0; i < ht->size; i++)
+{
+current = ht->array[i];
+if (current == NULL)
+continue;
+while (current != NULL)
+{
+free(current);
+current = current->next;
+}
+}
+free(ht);
+}
