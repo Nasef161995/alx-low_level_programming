0e8bcc1c7a023bcc53c4b0490ce97bd7607b3c69
Nasef161995~ <mnasef905@gmail.com>
2023-10-06 22:41:03 +0300

    33333

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,46 +4,43 @@
  * @ht: ...
  * @key: ...
  *@value: ...
- * Return: ....
- */
+* Return: ....
+*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *new;
-    hash_node_t *current;
-    unsigned long int index;
-    
-    new = malloc(sizeof(hash_node_t));
-    new->key = malloc(strlen(key) + 1);
-    new->value = malloc(strlen(value) + 1);
-    if (new == NULL || new->key ==NULL || new->value == NULL)
-        return (0);
-    strcpy(new->key, key);
-    strcpy(new->value, value);
-    new->next = NULL;
-
-    index = key_index((unsigned char *)key, ht->size);
-    current = ht->array[index];
-
-    if (current == NULL)
-    {
-        ht->array[index] = new;
-        return (1);
-    }
-    else
-    {
-        /* case1: Update the value.*/
-        if (strcmp(current->key,  key) == 0)
-        {
-            strcpy(ht->array[index]->value, value);
-            return (1);
-        }
-        else
-        {
-            /*case 2 : Handle the collision.*/
-            new->next = ht->array[index];
-            ht->array[index] = new;
-            return (1);
-        }
-    }
-    return (0);
+hash_node_t *new;
+hash_node_t *current;
+unsigned long int index;
+if (strlen(key) == 0)
+return (0);
+new = malloc(sizeof(hash_node_t));
+new->key = malloc(strlen(key) + 1);
+new->value = malloc(strlen(value) + 1);
+if (new == NULL || new->key ==NULL || new->value == NULL)
+return (0);
+strcpy(new->key, key);
+strcpy(new->value, value);
+new->next = NULL;
+index = key_index((unsigned char *)key, ht->size);
+current = ht->array[index];
+if (current == NULL)
+{
+ht->array[index] = new;
+return (1);
+}
+else
+{
+if (strcmp(current->key,  key) == 0)
+{
+strcpy(ht->array[index]->value, value);
+return (1);
+}
+else
+{
+new->next = ht->array[index];
+ht->array[index] = new;
+return (1);
+}
+}
+return (0);
 }
