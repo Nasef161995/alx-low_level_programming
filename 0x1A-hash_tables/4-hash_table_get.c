718b93f4e7d2291b0351cf3f938fcb3c2de2a446
Nasef161995~ <mnasef905@gmail.com>
2023-10-07 00:29:24 +0300

    444444

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - ...
+ * @ht: ...
+ * @key: ...
+ * Return: ...
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+hash_node_t *current;
+unsigned long int index;
+
+index = key_index((unsigned char *)key, ht->size);
+current = ht->array[index];
+if (current == NULL)
+return (NULL);
+
+while (current != NULL)
+{
+if (strcmp(current->key, key) == 0)
+{
+return (current->value);
+}
+current = current->next;
+
+}
+return (NULL);
+}
