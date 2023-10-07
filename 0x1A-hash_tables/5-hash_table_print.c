33a34297a8cc7158ef938aa5ff91ad8330fd6e1e
Nasef161995~ <mnasef905@gmail.com>
2023-10-07 08:42:16 +0300

    edit 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,19 +8,38 @@ void hash_table_print(const hash_table_t *ht)
 {
 hash_node_t *current;
 unsigned long int i;
+unsigned long int count;
 
+if (ht == NULL)
+return;
+for (i = ht->size; i != 0; i--)
+{
+if (ht->array[i])
+{
+count = i;
+break;
+}
+}
+
+printf("{");
 for (i = 0; i < ht->size; i++)
-{  
+{
+if (ht->array[i])
+{
 current = ht->array[i];
-if (current == NULL)
-continue;
-while (current != NULL)
+while (current)
+{
+if (current->next == NULL && (i == count))
 {
 printf("'%s' : '%s'", current->key, current->value);
+}
+else
+{
+printf("'%s' : '%s', ", current->key, current->value);
+}
 current = current->next;
 }
-
-printf(" , ");
 }
-
+}
+printf("}\n");
 }
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,21 +6,21 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-hash_node_t *current;
-unsigned long int i;
+    hash_node_t *current;
+    unsigned long int i;
 
-
-
-for (i = 0; i < ht->size; i++)
-{
-current = ht->array[i];
-if (current == NULL)
-continue;
-while (current != NULL)
-{
-free(current);
-current = current->next;
-}
-}
-free(ht);
+    for (i = 0; i < ht->size; i++)
+    {
+        current = ht->array[i];
+        if (current == NULL)
+            continue;
+        while (current != NULL)
+        {
+            free(current->key);
+            free(current->value);
+            free(current);
+            current = current->next;
+        }
+    }
+    free(ht);
 }
