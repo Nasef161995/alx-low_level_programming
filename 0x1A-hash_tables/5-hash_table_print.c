831f2f652022485401e1206122b3e097e165a2a8
Nasef161995~ <mnasef905@gmail.com>
2023-10-07 08:45:45 +0300

    edit 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,45 +1,47 @@
-#include "hash_tables.h"
-/**
- * hash_table_print - ...
- * @ht: ...
- * Return: ...
- */
-void hash_table_print(const hash_table_t *ht)
-{
-hash_node_t *current;
-unsigned long int i;
-unsigned long int count;
+    #include "hash_tables.h"
+    /**
+     * hash_table_print - ...
+     * @ht: ...
+     * Return: ...
+     */
+    void hash_table_print(const hash_table_t *ht)
+    {
+    hash_node_t *current;
+    unsigned long int i;
+    unsigned long int count;
 
-if (ht == NULL)
-return;
-for (i = ht->size; i != 0; i--)
-{
-if (ht->array[i])
-{
-count = i;
-break;
-}
-}
+    if (ht == NULL)
+    return;
+    for (i = ht->size; i != 0; i--)
+    {
+    if (ht->array[i])
+    {
+    count = i;
+    break;
+    }
+    }
 
-printf("{");
-for (i = 0; i < ht->size; i++)
-{
-if (ht->array[i])
-{
-current = ht->array[i];
-while (current)
-{
-if (current->next == NULL && (i == count))
-{
-printf("'%s' : '%s'", current->key, current->value);
-}
-else
-{
-printf("'%s' : '%s', ", current->key, current->value);
-}
-current = current->next;
-}
-}
-}
-printf("}\n");
-}
+    printf("{");
+    for (i = 0; i < ht->size; i++)
+    {
+    if (ht->array[i])
+    {
+    current = ht->array[i];
+    if (current == NULL)
+    break;
+    while (current)
+    {
+    if (current->next == NULL && (i == count))
+    {
+    printf("'%s' : '%s'", current->key, current->value);
+    }
+    else
+    {
+    printf("'%s' : '%s', ", current->key, current->value);
+    }
+    current = current->next;
+    }
+    }
+    }
+    printf("}\n");
+    }
