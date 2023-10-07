285b5cce3bf7bb128d172a7e15fb921d7e15c1d9
Nasef161995~ <mnasef905@gmail.com>
2023-10-07 11:36:18 +0300

    edit

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,14 +1,14 @@
 #include "hash_tables.h"
 /**
- * hash_table_delete - ...
- * @ht: ...
- * Return: ...
- */
+* hash_table_delete - ...
+* @ht: ...
+* Return: ...
+*/
 void hash_table_delete(hash_table_t *ht)
 {
 hash_node_t *current, *temp;
 unsigned long int i;
-if (!ht || ht->size == 0)
+if (ht == NULL)
 return;
 
 for (i = 0; i < ht->size; i++)
@@ -21,9 +21,12 @@ free(current->key);
 free(current->value);
 free(current);
 current = temp;
+if (current)
+ht->array[i] = NULL;
 }
 }
 free(ht->array);
 ht->array = NULL;
 free(ht);
+ht = NULL;
 }
