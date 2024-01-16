9aa64fca620f50198a9910cfb1f579284db0eeb9
Nasef161995~ <mnasef905@gmail.com>
2024-01-16 14:51:24 +0300

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,7 +12,14 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	for (size_t i = 0; i < size; i++)
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
 	{
 		if (array[i] == value)
 		{
