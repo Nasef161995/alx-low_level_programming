2fff310be4449322767a4e5c56b7f2eb8f759810
Nasef161995~ <mnasef905@gmail.com>
2024-01-16 14:55:07 +0300

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,11 +21,12 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
+
+		printf("Value checked array[%ld] = [%d] \n", i, array[i]);
 		if (array[i] == value)
 		{
 			return (i);
 		}
-		printf("Value checked array[%ld] = [%d] \n", i, array[i]);
 	}
 	return (-1);
 }
